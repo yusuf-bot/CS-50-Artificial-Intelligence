a5cc559db63fc12f170304c5b92027562af91fa7
Yusuf <111415081+yusuf-bot@users.noreply.github.com>
2023-07-06 16:11:10 +0400

    Add files via upload

---
+++ parser/Parser0.py
@@ -0,0 +1,90 @@
+import nltk
+import sys
+
+TERMINALS = """
+Adj -> "country" | "dreadful" | "enigmatical" | "little" | "moist" | "red"
+Adv -> "down" | "here" | "never"
+Conj -> "and" | "until"
+Det -> "a" | "an" | "his" | "my" | "the"
+N -> "armchair" | "companion" | "day" | "door" | "hand" | "he" | "himself"
+N -> "holmes" | "home" | "i" | "mess" | "paint" | "palm" | "pipe" | "she" 
+N -> "smile" | "thursday" | "walk" | "we" | "word"
+P -> "at" | "before" | "in" | "of" | "on" | "to"
+V -> "arrived" | "came" | "chuckled" | "had" | "lit" | "said" | "sat"
+V -> "smiled" | "tell" | "were"
+"""
+
+NONTERMINALS = """
+S -> NP VP | S Conj S | S Conj VP | S Conj NP
+AP -> Adj | Adj AP
+NP -> N | Det NP | NP PP | Det AP NP | NP P NP | AP NP | PP NP 
+PP -> PP NP | P
+VP -> V | VP NP | VP PP | Adv VP | VP Adv
+"""
+
+grammar = nltk.CFG.fromstring(NONTERMINALS + TERMINALS)
+parser = nltk.ChartParser(grammar)
+
+
+def main():
+
+    # If filename specified, read sentence from file
+    if len(sys.argv) == 2:
+        with open(sys.argv[1]) as f:
+            s = f.read()
+
+    # Otherwise, get sentence as input
+    else:
+        s = input("Enter Sentence here: ")
+
+
+    # Convert input into list of words
+    s = preprocess(s)
+
+    # Attempt to parse sentence
+    try:
+        trees = list(parser.parse(s))
+    except ValueError as e:
+        print(e)
+        return
+    if not trees:
+        print("Could not parse sentence.")
+        return
+
+
+    # Print each tree with noun phrase chunks
+    for tree in trees:
+        tree.pretty_print()
+
+
+
+
+def preprocess(sentence):
+    """
+    Convert `sentence` to a list of its words.
+    Pre-process sentence by converting all characters to lowercase
+    and removing any word that does not contain at least one alphabetic
+    character.
+    """
+    New_sentence=[]
+    tokenized_text = nltk.word_tokenize(sentence)
+    for word in tokenized_text:
+        new_word=word
+        for letter in word:
+            if ord(letter)>=ord('A') and ord(letter)<=ord('Z'):
+                new_word = word.translate( { ord(letter): ord(letter.lower()) } )
+                letter=letter.lower()
+            if ord(letter)<ord('a') or ord(letter)>ord('z'):
+                new_word = word.translate( { ord(letter): None } )
+
+        New_sentence.append(new_word)
+    for word in New_sentence:
+        if word=='':
+            New_sentence.remove(word)
+
+    return (New_sentence)
+
+
+
+if __name__ == "__main__":
+    main()
